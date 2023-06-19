8d265ebfde0a4785a8b51627f9d809010cf657a3
Paul Dejean <paulcdejean@gmail.com>
2023-06-19 16:57:12 -0500

    Another TODO done.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import type { NS } from "@ns";
 import { noodle } from '@/n00dles.ts'
 
 export async function main(ns: NS): Promise<void> {
-  noodle(ns)
+  await noodle(ns)
   ns.tprint("Hello typescript!");
 }
