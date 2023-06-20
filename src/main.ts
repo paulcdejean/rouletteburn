068f582743b1abd1a421036dffcdd2b7ac04caba
Paul Dejean <paulcdejean@gmail.com>
2023-06-19 21:49:18 -0500

    More TODOs

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,25 @@ import { fastestResults } from "@/targettingAlgos/fastestResults.ts"
 
 export async function main(ns: NS): Promise<void> {
   const network = refreshNetwork(ns, Capabilities.Basic)
+
+  // TODO
+  // crack(ns, network)
+
+  // async farm loop
+  /*
+  pick a targetting algo
+    this will be heurestic nonsense
+  pick a farming algo
+    just hwgw until later
+  prep server if not already prepped
+    loop here
+  farm server if its prepped
+    then loop
+  */
+
+  // async cracking loop
+
+  // async quest loop (react time lol)
   
   ns.tprint(fastestResults(ns, network))
 }
