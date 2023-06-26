c23ded117541713fbccb197f6b11d3242a3e2849
Paul Dejean <paulcdejean@gmail.com>
2023-06-25 22:19:01 -0500

    Depreciated function bluh bluh

--- src/targettingAlgos/metaTargeting.ts
+++ src/targettingAlgos/metaTargeting.ts
@@ -1,15 +1,11 @@
 import { Capabilities } from "@/Capabilities"
 import type { NS } from "@ns";
 import { Network } from "@/network"
-import { largestMaxMoneyPerSecondUnderThreeMinutes, fastestResults } from "@/targettingAlgos/heuristics";
+import { largestMaxMoneyPerSecondUnderThreeMinutes } from "@/targettingAlgos/heuristics";
 
 type TargettingAlgo = (ns: NS, network: Network) => string
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function metaTargetting(ns: NS, capabilities: Capabilities) : TargettingAlgo {
-  if (ns.getTimeSinceLastAug() < 300000) {
-    return fastestResults
-  } else {
-    return largestMaxMoneyPerSecondUnderThreeMinutes
-  }
+  return largestMaxMoneyPerSecondUnderThreeMinutes
 }
