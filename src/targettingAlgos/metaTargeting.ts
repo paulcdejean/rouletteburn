267d9a566eb83469863872ca8aa67bddda47b92c
Paul Dejean <paulcdejean@gmail.com>
2023-06-25 22:01:10 -0500

    New targetting algorithm.

--- src/targettingAlgos/metaTargeting.ts
+++ src/targettingAlgos/metaTargeting.ts
@@ -2,10 +2,15 @@ import { Capabilities } from "@/Capabilities"
 import type { NS } from "@ns";
 import { Network } from "@/network"
 import { fastestResults } from "@/targettingAlgos/fastestResults"
+import { largestUnderThreeMinutes } from "./largestUnderThreeMinutes";
 
 type TargettingAlgo = (ns: NS, network: Network) => string
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function metaTargetting(ns: NS, capabilities: Capabilities) : TargettingAlgo {
-  return fastestResults
+  if (ns.getTimeSinceLastAug() < 300000) {
+    return fastestResults
+  } else {
+    return largestUnderThreeMinutes
+  }
 }
