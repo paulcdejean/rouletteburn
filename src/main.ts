5411e693732a08db6d4eba126ea8b36f69fb948e
Paul Dejean <paulcdejean@gmail.com>
2023-07-03 21:47:46 -0500

    Tidy.

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,26 @@ import RouletteHelper from "@/ui/RouletteHelper/RouletteHelper";
 import { sleep } from "./utils";
 
 export async function main(ns: NS): Promise<void> {
-  ns.clearLog()
+  roulette(ns)
+
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition, no-constant-condition
+  while(true) {
+    await sleep(2000)
+  }
+}
+
+export function roulette(ns: NS): void {
   ns.disableLog("ALL")
+  ns.clearLog()
   // Cleans up react element after exit
-  ns.atExit(() => ns.clearLog())
+  ns.atExit(() => {
+    ns.clearLog()
+    ns.closeTail()
+  })
 
   ns.printRaw(React.createElement(RouletteHelper))
   
   ns.tail()
   ns.resizeTail(750, 400)
   ns.moveTail(350, 550)
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition, no-constant-condition
-  while(true) {
-    await sleep(2000)
-  }
 }
